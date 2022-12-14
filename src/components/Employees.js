93e871e647f761dd0fcbd6603d1ac4ecc99a68ac
Aneesh unni <a8156904782@gmail.com>
2022-12-14 11:59:09 +0530

    addedit

---
+++ src/components/Employees.js
@@ -0,0 +1,21 @@
+
+
+const Employees= [
+    {
+        id:"1",
+        Name:"aneesh unni",
+        Age:"24"
+    },
+    {
+        id:"2",
+        Name:"anu",
+        Age:"21"
+    },
+    {
+        id:"3",
+        Name:"anumon",
+        Age:"27"
+    }
+]
+
+export default Employees
---
+++ src/components/Home.js
@@ -0,0 +1,70 @@
+import React, { Fragment } from 'react'
+import {Button,Table} from 'react-bootstrap'
+import Employees from './Employees'
+import {Link,useNavigate} from 'react-router-dom'
+
+const Home = () => {
+
+let history = useNavigate();
+    const handleDelete=(id)=>{
+        var index = Employees.map(function(e){
+            return e.id
+        }).indexOf(id);
+
+        Employees.splice(index,1);
+        history('/');
+    }
+
+  return (
+    <Fragment>
+        <div style={{margin:"10rem"}}>
+            <Table striped bordered hover size='sm'>
+                <thead>
+                    <tr>
+                        <th>
+                            Name
+                        </th>
+                        <th>
+                            Age
+                        </th>
+                        <th>
+                            Actions
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        Employees && Employees.length > 0
+                        ?
+                        Employees.map((item)=>{
+                            return(
+                                <tr>
+                                    <td>
+                                        {item.Name}
+                                    </td>
+                                    <td>
+                                        {item.Age}
+                                    </td>
+                                    <td>
+                                        <Link to={'/edit'}>
+                                        <Button onClick={()=>alert(item.id)}>Edit</Button>
+                                        </Link>                                        &nbsp;&nbsp;
+                                        <Button onClick={()=>handleDelete(item.id)}>Delete</Button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                        :"No data found"
+                    }
+                </tbody>
+            </Table>
+            <br></br>
+            <Link className='d-grid gap-2' to={'/create'}>
+                <Button size='lg'>Create</Button>
+                </Link>
+        </div>
+    </Fragment>
+  )
+}
+
+export default Home
